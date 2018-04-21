709ef1c9b927c9bc9ba9a33d2af7758a770a2390
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-21 13:00:29 +0200

    reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+	return [
+		{title: 'Javascript: The Good Parts'},
+		{title: 'The Dark Tower'},
+		{title: 'Eloquent Javascript'},
+		{title: 'TDD by Example'},
+		{title: 'Learning Reactjs'}
+	]
+}
