fe1a3808183688e2339b77caa71e80c9d2b5e6e1
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-21 19:44:09 +0200

    book detail

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import BookList from '../containers/book-list';
+import BookDetail from '../containers/book-detail';
 
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
 		return (
 			<div>
 				<BookList />
+				<BookDetail />
 			</div>
 		);
 	}
